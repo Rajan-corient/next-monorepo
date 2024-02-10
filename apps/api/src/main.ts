1c2efc9be5a469acebb833b06041c87c8de9b5b0
Rajan Gupta <31067215+Rajan-corient@users.noreply.github.com>
2024-02-10 16:12:04 +0530

    shared types and data-access library added

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -5,13 +5,36 @@
 
 import express from 'express';
 import * as path from 'path';
+import { pokemonList } from './pokemon';
+import cors from 'cors';
+
+import { pokemon } from '@nx-nextjs-monorepo/shared-types';
 
 const app = express();
-
+app.use(cors());
+// Parse JSON bodies (req.body)
+app.use(express.json());
+// Priority serve any static files.
 app.use('/assets', express.static(path.join(__dirname, 'assets')));
 
 app.get('/api', (req, res) => {
   res.send({ message: 'Welcome to api!' });
+});
+
+app.get('/pokemon', (req, res) => {
+  res.send(pokemonList);
+});
+
+app.get('/search', (req, res) => {
+  const searchTerm = (req.query.name as string)?.toString().toLowerCase() || '';
+  console.log('searchTerm', searchTerm);
+
+  if (!searchTerm) return res.status(400).send('Missing search term');
+
+  const filteredPokemon: pokemon[] = pokemonList.filter(({ name }) =>
+    name.toLowerCase().includes(searchTerm)
+  );
+  res.send(filteredPokemon);
 });
 
 const port = process.env.PORT || 3333;
